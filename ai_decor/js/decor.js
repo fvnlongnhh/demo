922a13036d58f70226a50265d6146c4104d2f8a8
LongNHH <fvn.longnhh@gmail.com>
2020-03-06 11:26:34 +0700

    done product

--- ai_decor/js/decor.js
+++ ai_decor/js/decor.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+$(document).ready(function() {
     "use strict";
-    
+
     $(".function_aside span").click(function() {
         $('.modal_price').addClass('open');
     });
@@ -10,15 +10,15 @@ $(document).ready(function () {
     });
 
     $(window).scroll(function() {
-        if($(window).width() > 769){
+        if ($(window).width() > 769) {
             var h_head = $('.head_ct').outerHeight();
             if ($(this).scrollTop() > 200) {
                 $('.head_ct').addClass('fixed')
-                $('.head_top').css({'margin-bottom': 120});
+                $('.head_top').css({ 'margin-bottom': 120 });
 
             } else {
                 $('.head_ct').removeClass('fixed')
-                $('.head_top').css({'margin-bottom': 0});
+                $('.head_top').css({ 'margin-bottom': 0 });
             }
 
             if ($(this).scrollTop() > 300) {
@@ -29,11 +29,11 @@ $(document).ready(function () {
         }
     });
 
-    $('.btn_top').click(function () {
-        $('body,html').animate({scrollTop: 0}, 800);
+    $('.btn_top').click(function() {
+        $('body,html').animate({ scrollTop: 0 }, 800);
     });
 
-    $(".btn_side").click(function () {
+    $(".btn_side").click(function() {
         $(this).toggleClass('open');
         $("nav").toggleClass("active");
         $(".sub a").removeClass("disable");
@@ -41,13 +41,34 @@ $(document).ready(function () {
         return false;
     });
 
-    $("li.sub span").click(function(){
+    if ($(window).width() < 769) {
+
+        $('.nav_2 ul').hide();
+        $(".nav_2 p").click(function() {
+            $(this).toggleClass('active');
+            $(this).next('ul').slideToggle(400);
+        });
+
+        $(".nav_2 li.sub > span").click(function() {
+            $(this).toggleClass('active');
+            $(this).parent().find('.nav_sub').slideToggle(400);
+        });
+
+        $(".nav_2 li.sub_lv2 span").click(function() {
+            $(this).toggleClass('active');
+            $(this).parent().find('.nav_sub_lv2').slideToggle(400);
+        });
+
+    }
+
+    $("nav li.sub span").click(function() {
         $(this).parent().find('.nav_sub').slideToggle(400);
     });
 
-    $("li.sub_lv2 span").click(function(){
+    $("nav li.sub_lv2 span").click(function() {
         $(this).parent().find('.nav_sub_lv2').slideToggle(400);
     });
+
 
     $('.foot_partner ul').slick({
         dots: false,
@@ -55,43 +76,40 @@ $(document).ready(function () {
         slidesToShow: 5,
         speed: 500,
         pauseOnHover: false,
-        pauseOnFocuse : false,
+        pauseOnFocuse: false,
         margin: 0,
         autoplay: true,
         autoplaySpeed: 5000,
-        responsive: [
-        {
-          breakpoint: 769,
-          settings: {
-            slidesToShow: 4,
-          }
-        },
-        {
-          breakpoint: 481,
-          settings: {
-            slidesToShow: 3,
-          }
-        }
+        responsive: [{
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
         ]
     });
 
 
-    if( $('.prd_detail_img').length){
-         $('.slide_big').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          fade: true,
-          asNavFor: '.slide_thumb'
+    if ($('.prd_detail_img').length) {
+        $('.slide_big').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slide_thumb'
         });
         $('.slide_thumb').slick({
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          asNavFor: '.slide_big',
-          dots: false,
-          focusOnSelect: true
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slide_big',
+            dots: false,
+            focusOnSelect: true
         });
     }
 });
-
-
